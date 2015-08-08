af03161cc135a449b097b2d74f1878c186e94294
Amiya Sahu <developer.amiya@outlook.com>
2015-08-08 23:36:53 +0530

    added function exists checks

--- qa-breadcrumbs-utils.php
+++ qa-breadcrumbs-utils.php
@@ -32,91 +32,110 @@
         exit;
     }
 
-    /**
-     * Returns the language html value as defined in lang file
-     *
-     * @param      $indentifier
-     * @param null $subs
-     *
-     * @return mixed|string
-     */
-    function breadcrumb_lang( $indentifier, $subs = null )
-    {
-        if ( !is_array( $subs ) )
-            return empty( $subs ) ? qa_lang_html( 'breadcrumbs/' . $indentifier ) : qa_lang_html_sub( 'breadcrumbs/' . $indentifier, $subs );
-        else
-            return strtr( qa_lang_html( 'breadcrumbs/' . $indentifier ), $subs );
+    if ( !function_exists( 'breadcrumb_lang' ) ) {
+        /**
+         * Returns the language html value as defined in lang file
+         *
+         * @param      $indentifier
+         * @param null $subs
+         *
+         * @return mixed|string
+         */
+        function breadcrumb_lang( $indentifier, $subs = null )
+        {
+            if ( !is_array( $subs ) )
+                return empty( $subs ) ? qa_lang_html( 'breadcrumbs/' . $indentifier ) : qa_lang_html_sub( 'breadcrumbs/' . $indentifier, $subs );
+            else
+                return strtr( qa_lang_html( 'breadcrumbs/' . $indentifier ), $subs );
+        }
     }
 
-    /**
-     * Returns the first element in an array.
-     *
-     * @param  array $array
-     * @return mixed
-     */
+    if ( !function_exists( '!' ) ) {
+        /**
+         * Returns the first element in an array.
+         *
+         * @param  array $array
+         * @return mixed
+         */
 
-    function array_first( array $array )
-    {
-        return reset( $array );
+        function array_first( array $array )
+        {
+            return reset( $array );
+        }
+
     }
 
-    /**
-     * Returns the last element in an array.
-     *
-     * @param  array $array
-     * @return mixed
-     */
-    function array_last( array $array )
-    {
-        return end( $array );
+    if ( !function_exists( 'array_last' ) ) {
+        /**
+         * Returns the last element in an array.
+         *
+         * @param  array $array
+         * @return mixed
+         */
+        function array_last( array $array )
+        {
+            return end( $array );
+        }
     }
 
-    /**
-     * Returns the first key in an array.
-     *
-     * @param  array $array
-     * @return int|string
-     */
-    function array_first_key( array $array )
-    {
-        reset( $array );
 
-        return key( $array );
+    if ( !function_exists( 'array_first_key' ) ) {
+        /**
+         * Returns the first key in an array.
+         *
+         * @param  array $array
+         * @return int|string
+         */
+        function array_first_key( array $array )
+        {
+            reset( $array );
+
+            return key( $array );
+        }
     }
 
-    /**
-     * Returns the last key in an array.
-     *
-     * @param  array $array
-     * @return int|string
-     */
-    function array_last_key( array $array )
-    {
-        end( $array );
 
-        return key( $array );
+    if ( !function_exists( 'array_last_key' ) ) {
+        /**
+         * Returns the last key in an array.
+         *
+         * @param  array $array
+         * @return int|string
+         */
+        function array_last_key( array $array )
+        {
+            end( $array );
+
+            return key( $array );
+        }
     }
 
-    /**
-     * Checks wheather the provided key is the first key of the array
-     *
-     * @param array $array
-     * @param $key
-     * @return bool
-     */
-    function array_is_first_key( array $array, $key )
-    {
-        return array_first_key( $array ) == $key;
+
+    if ( !function_exists( 'array_is_first_key' ) ) {
+        /**
+         * Checks wheather the provided key is the first key of the array
+         *
+         * @param array $array
+         * @param $key
+         * @return bool
+         */
+        function array_is_first_key( array $array, $key )
+        {
+            return array_first_key( $array ) == $key;
+        }
     }
 
-    /**
-     * Checks wheather the provided key is the first key of the array
-     *
-     * @param array $array
-     * @param $key
-     * @return bool
-     */
-    function array_is_last_key( array $array, $key )
-    {
-        return array_last_key( $array ) == $key;
+
+    if ( !function_exists( 'array_is_last_key' ) ) {
+        /**
+         * Checks wheather the provided key is the first key of the array
+         *
+         * @param array $array
+         * @param $key
+         * @return bool
+         */
+        function array_is_last_key( array $array, $key )
+        {
+            return array_last_key( $array ) == $key;
+        }
     }
