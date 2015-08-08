0888eb03397b620422c3337cdfc7bd1568eda9a1
Amiya Sahu <developer.amiya@outlook.com>
2015-08-08 17:03:01 +0530

    removed unwanted code

--- qa-breadcrumbs-layer.php
+++ qa-breadcrumbs-layer.php
@@ -52,15 +52,8 @@
         {
             qa_html_theme_base::head_css();
             if ( $this->template != 'admin' ) {
-                $breadcrumb_css_url = qa_opt( 'site_url' ) . 'qa-plugin/' . AMI_BREADCRUMBS_FOLDER . '/css/breadcrumbs-styles.css';
+                $breadcrumb_css_url = qa_path_to_root() . 'qa-plugin/' . AMI_BREADCRUMBS_FOLDER . '/css/breadcrumbs-styles.css';
                 $this->output( '<link rel="stylesheet" TYPE="text/css" href="' . $breadcrumb_css_url . '"/>' );
-                $fa_cdn = qa_opt( q2a_breadcrumbs_admin::FA_CDN );
-                if ( qa_opt( q2a_breadcrumbs_admin::USE_FA_CDN ) && !empty( $fa_cdn ) ) {
-                    $this->output( '<link rel="stylesheet" TYPE="text/css" href="' . $fa_cdn . '"/>' );
-                } else {
-                    $icons = qa_opt( 'site_url' ) . 'qa-plugin/' . AMI_BREADCRUMBS_FOLDER . '/css/font-awesome.min.css';
-                    $this->output( '<link rel="stylesheet" TYPE="text/css" href="' . $icons . '"/>' );
-                }
             }
         }
     }
