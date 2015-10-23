85d688524b86b27be3e79d8d77d750d89798589f
Amiya Sahu <amiya053@gmail.com>
2015-10-23 21:46:18 +0530

    Update qa-plugin.php

--- qa-plugin.php
+++ qa-plugin.php
@@ -29,8 +29,8 @@
         Plugin Name: Q2A Breadcrumbs
         Plugin URI: https://github.com/amiyasahu/q2a-breadcrumbs
         Plugin Description: Provides a basic widget for displaying breadcrumbs
-        Plugin Version: 1.5
-        Plugin Date: 2015-08-09
+        Plugin Version: 1.5.1
+        Plugin Date: 2015-10-23
         Plugin Author: Amiya Sahu
         Plugin Author URI: http://www.amiyasahu.com/
         Plugin License: GPLv2
